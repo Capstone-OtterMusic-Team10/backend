38b5c06add2ed8fd9ee139cddcd6a4b8b1d2c22f
Pol <zhperon@gmail.com>
2025-07-08 11:17:45 -0700

    corrected filepath and such

--- app/chats/routes.py
+++ app/chats/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request, make_response, send_file
-from ..models import Chat, Messages
+from ..models import Chat, Messages, Audios
 from .. import db
 from app.chats.lyria_demo_test2 import generate_audio
 import os
@@ -24,8 +24,9 @@ def commit(new_obj, action = "add"):
 
 
 def create_a_message_and_send_prompt(prompt, chat_id, data, prompt_id):
-    print(data)
     asyncio.run(generate_audio(data["bpm"], data["key"], prompt, chat_id, prompt_id))
+    new_audio = Audios(link=f"lyria_{chat_id}_{prompt_id}", chat=chat_id, prompt=prompt_id)
+    commit(new_audio)
 
 # chat functionality
 @routes_bp.route('/chat')
@@ -89,24 +90,28 @@ def post_chats():
         
         Thread(target=create_a_message_and_send_prompt, args=(new_exchange.content, new_chat.id, data, new_exchange.id)
 ).start()
-        return make_response(jsonify({"new_chat": new_chat.id}), 200)
+        return make_response(jsonify({"new_chat": new_chat.id, "new_message": new_exchange.id}), 200)
     # Here we would have a process that gives us music
     else:
         new_exchange = Messages(role="user", content=data["prompt"], convo=data["chat"])
         commit(new_exchange)
         
         Thread(target=create_a_message_and_send_prompt, args=(new_exchange.content, data["chat"], data, new_exchange.id)).start()
-        return make_response(jsonify({"message": "New message created"}), 200)
+        return make_response(jsonify({"message": new_exchange.id}), 200)
     
 @routes_bp.route('/get-audio/<int:chat_id>/<int:message_id>')
 def get_audio(chat_id, message_id):
-    file_path = f'/home/pol/projects/caps/backend/MusicDownloadFiles/lyria_{chat_id}_{message_id}.wav'
-    try :
+    file_path = f'{music_folder}/lyria_{chat_id}_{message_id}.wav'
+    try:
         return send_file(file_path, mimetype='audio/wav')
     except FileNotFoundError:
         return make_response(jsonify({'message': 'No audio available'}), 404)
    
 
+# getting all audios (will need to be refactred by user-basis once auth is implemented)
+@routes_bp.route('/all-audios/<int:user_id>')
+def get_audios(user_id):
+    my_chats = Chat.query.get()
 @routes_bp.route('/getmessages/<int:id>')
 def get_messages(id):
     try:
